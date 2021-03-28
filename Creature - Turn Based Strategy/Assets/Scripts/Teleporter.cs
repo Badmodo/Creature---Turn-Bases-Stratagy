0538cc9ee78eccdde4840911cbceb7ac7bfd4594
Sam <samleitch75@gmail.com>
2021-03-28 21:55:38 +1100

    Further attempts to set up Teleporter

--- Creature - Turn Based Strategy/Assets/Scripts/Teleporter.cs
+++ Creature - Turn Based Strategy/Assets/Scripts/Teleporter.cs
@@ -10,7 +10,19 @@ public class Teleporter : MonoBehaviour
     public Transform FireTarget;
     public Rigidbody player;
 
+    public GameObject GrassBits;
+    public GameObject WaterBits;
+    public GameObject SandBits;
+    public GameObject FireBits;
+
+    public GameObject GrassBackground;
+    public GameObject WaterBackground;
+    public GameObject SandBackground;
+    public GameObject FireBackground;
+
     int currentAction;
+
+    [SerializeField] BattleDialogueBox dialogueBox;
 
 
     //private void Update()
@@ -25,6 +37,54 @@ public class Teleporter : MonoBehaviour
     //{
     //    player.transform.position = teleportTarget.position;
     //}
+
+    private void Update()
+    {
+        if(GrassBits == true)
+        {
+            GrassBackground.SetActive(true);
+            WaterBackground.SetActive(false);
+            SandBackground.SetActive(false);
+            FireBackground.SetActive(false);
+
+            WaterBits.SetActive(false);
+            SandBits.SetActive(false);
+            FireBits.SetActive(false);
+        }
+        else if(WaterBits == true)
+        {
+            GrassBackground.SetActive(false);
+            WaterBackground.SetActive(true);
+            SandBackground.SetActive(false);
+            FireBackground.SetActive(false);
+
+            GrassBits.SetActive(false);
+            SandBits.SetActive(false);
+            FireBits.SetActive(false);
+        }
+        else if(SandBits == true)
+        {
+            GrassBackground.SetActive(false);
+            WaterBackground.SetActive(false);
+            SandBackground.SetActive(true);
+            FireBackground.SetActive(false);
+
+            GrassBits.SetActive(false);
+            WaterBits.SetActive(false);
+            FireBits.SetActive(false);
+        }
+        else if(FireBits == true)
+        {
+            GrassBackground.SetActive(false);
+            WaterBackground.SetActive(false);
+            SandBackground.SetActive(false);
+            FireBackground.SetActive(true);
+
+            GrassBits.SetActive(false);
+            SandBits.SetActive(false);
+            WaterBits.SetActive(false);
+        }
+    }
 
     void HandleTeleportAction()
     {
@@ -47,7 +107,7 @@ public class Teleporter : MonoBehaviour
 
         currentAction = Mathf.Clamp(currentAction, 0, 3);
 
-        //dialogueBox.UpdateActionSelection(currentAction);
+        dialogueBox.UpdateActionSelection(currentAction);
 
         //change states either move list or run away
         if (Input.GetKeyDown(KeyCode.Z))
@@ -55,21 +115,25 @@ public class Teleporter : MonoBehaviour
             if (currentAction == 0)
             {
                 //0 = Teleport to this tower
+                GrassBits.SetActive(true);
                 player.transform.position = GrassTarget.position;
             }
             else if (currentAction == 1)
             {
                 //1 = Teleport to this tower
+                WaterBits.SetActive(true);
                 player.transform.position = WaterTarget.position;
             }
             else if (currentAction == 2)
             {
                 //2 = Teleport to this tower
+                SandBits.SetActive(true);
                 player.transform.position = SandTarget.position;
             }
             else if (currentAction == 3)
             {
                 //3 = Teleport to this tower
+                FireBits.SetActive(true);
                 player.transform.position = FireTarget.position;
             }
         }
