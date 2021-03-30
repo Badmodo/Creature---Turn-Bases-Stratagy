41d023214a680f0839e8019770f46bb8e593554d
Sam <samleitch75@gmail.com>
2021-03-30 12:59:39 +1100

    AnimationUpdate

---
+++ Creature - Turn Based Strategy/Assets/Scripts/Movment/EnemyAi.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] GameObject SeePlayer;
+    [SerializeField] GameObject TeleportActivation;
+
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIdGround, whatIsPlayer;
+    [SerializeField] TrainerController trainer;
+
+  
+
+    public static float health;
+
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+    }
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //check to see if the player is within sight or attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        SeePlayer.SetActive(false);
+
+        //calculate a random point range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIdGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        trainer.TriggerTrainerBattle();
+
+        SeePlayer.SetActive(true);
+
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //make sure the enemy dosnt move on attack
+        agent.SetDestination(transform.position);
+
+        //look at player
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            //attack code here
+            GameController.Instance.StartTrainerBattle(trainer);
+            
+            //has the enemy attacked
+            alreadyAttacked = true;
+        }
+        else
+        {
+            DestroyEnemy();
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        TeleportActivation.SetActive(true);
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //this will visualize the attack and sight range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
---
+++ Creature - Turn Based Strategy/Assets/Scripts/TeleporterActivator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterActivator : MonoBehaviour
+{
+    public static bool canTeleport = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        canTeleport = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canTeleport = false;
+    }
+
+   
+}
